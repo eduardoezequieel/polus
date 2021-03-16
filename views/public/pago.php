94c1b0bc3538e0dcdba4f62a9c0158cd4d8c082c
Katherine Gonźalez <katy06salinas@gmail.com>
2021-03-16 02:04:58 -0600

    Retoques de pago

--- views/public/pago.php
+++ views/public/pago.php
@@ -16,9 +16,6 @@ public_Page::navbarTemplate('Polus El Salvador','index_publico_styles.css');
 			    <div class="col-12 tituloPrincipal">
 				    <h2 id="titulos">Pago</h2>
 			    </div>
-        </div>
-        <!-- Inicio de sección principal -->
-        <div class="row">
           <!-- Inicio de dirección  -->
           <div class="col-12">
             <br><p>¿Adónde se dirige el pedido?</p>
@@ -30,9 +27,6 @@ public_Page::navbarTemplate('Polus El Salvador','index_publico_styles.css');
             <p>Información de contácto:</p>
             <textarea cols="50" rows="2" class="form-control  personalizacionPolus" placeholder="Información del contacto en la base de datos"></textarea>
           </div>
-        </div>
-        <!-- Inicio del sección confirmar compra -->
-        <div class="row">
           <div class="col-12 pago">
             <a href="">
               <button class="btn btn-outline-light centrarImagen4">Confirmar compra</button>
@@ -44,6 +38,10 @@ public_Page::navbarTemplate('Polus El Salvador','index_publico_styles.css');
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+
+
+    <!-- IONICONS -->
+    <script src="https://unpkg.com/ionicons@5.4.0/dist/ionicons.js"></script>
 
   </body>
 </html>
