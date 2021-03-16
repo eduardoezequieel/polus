48ce9dbafcf88d5d18bf87c8293f44f014a98963
Katherine Gonźalez <katy06salinas@gmail.com>
2021-03-15 20:24:50 -0600

    Reseña con responsive

--- views/public/reseña.php
+++ views/public/reseña.php
@@ -1,51 +1,66 @@
-<?php
-//Se incluye la clase con las plantillas del documento
-include('../../app/helpers/public_page.php');
-//Se imprime la plantilla del encabezado y se envía el titulo para la página web
-public_Page::navbarTemplate('Polus El Salvador');
-?>
-<link rel="stylesheet" type="text/css" href="../../resources/css/reseña_publico.css">
-<section>
-    <div class="container-fluid" style="background-color: #22242C;">
+<!doctype html>
+<html lang="es">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+
+    <!-- Fuentes -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">          
+    <link href="https://fonts.googleapis.com/css2?family=Reem+Kufi&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300&display=swap" rel="stylesheet">
+
+    <!-- Hoja de Estilos -->
+    <link rel="stylesheet" type="text/css" href="../../resources/css/reseña_publico_estilos.css">
+    <title>Polus - Iniciar Sesión</title>
+  </head>
+  <body>
+    <!-- Fondo -->
+    <div id="background">
+      <!-- Caja -->
+      <div id="form" class="paddingLados">
         <div class="row">
-            <h1 class="titulos2" id="parrafo" style="background-color: #22242C;">
-                <spanstyle="font-weight:bolder">Escribe tu reseña</span>
-            </h1>
-            <center>
-                <div class="form col-md-6">
-                    <input type="" class="form-control" placeholder="Escribe aqui:">
-                    <h4>Valora tu experiencia</h4>
-                    <h1 class="titulos2" id="parrafo" style="background-color: #22242C;">
-                        <spanstyle="font-weight:bolder">Valora tu experiencia con nosotros</span>
-                    </h1>
-                    <div class="valoracion">
-                        <input id="radio1" type="radio" name="estrellas" value="5">
-                        <label for="radio1">★</label>
-                        <input id="radio2" type="radio" name="estrellas" value="4">
-                        <label for="radio2">★</label>
-                        <input id="radio3" type="radio" name="estrellas" value="3">
-                        <label for="radio3">★</label>
-                        <input id="radio4" type="radio" name="estrellas" value="2">
-                        <label for="radio4">★</label>
-                        <input id="radio5" type="radio" name="estrellas" value="1">
-                        <label for="radio5">★</label>
-                    </div>
-                    <button class="btn btn-outline-light centrarImagenes2" style="margin-top:40px;">Enviar Reseña</button><br><br>
+            <!-- Inicio de reseña -->
+            <div col="col-12">
+                <h1 class="titulos4" id="parrafo">
+                    Escribe tu reseña
+                </h1>
+            </div>
+            <!-- Inicio de puntuación -->
+            <div class="form col-12">
+                <textarea cols="30" rows="5" class="form-control personalizacionPolus" placeholder="Escribe aquí..."></textarea>
+                <br>
+                <h2 class="titulos5" id="parrafo">
+                    Valora tu experiencia con nosotros
+                </h2>
+                <div class="col-lg-12 valoracion">
+                     <input id="radio1" type="radio" name="estrellas" value="1">
+                      <label for="radio1">★</label>
+                      <input id="radio2" type="radio" name="estrellas" value="2">
+                      <label for="radio2">★</label>
+                      <input id="radio3" type="radio" name="estrellas" value="3">
+                      <label for="radio3">★</label>
+                      <input id="radio4" type="radio" name="estrellas" value="4">
+                      <label for="radio4">★</label>
+                      <input id="radio5" type="radio" name="estrellas" value="5">
+                      <label for="radio5">★</label>
                 </div>
-            </center>
+                <!-- Inicio de botón -->
+                <div class="col-12 boton3">
+                    <button class="btn btn-outline-light centrarImagenes2">Enviar Reseña</button><br><br>
+                </div>
+            </div>
         </div>
-    </div>
-</section>
-<div class="media">
-    <div class="media-body">
-
-
+      </div>
     </div>
 
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
 
-</div>
-
-
-<?php
-public_Page::footerTemplate();
-?>
+  </body>
+</html>
