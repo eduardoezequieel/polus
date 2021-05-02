141e2a5edb59075d3b0dc5d1e348150fe0d04b9c
Katherine Gonźalez <katy06salinas@gmail.com>
2021-05-01 22:35:16 -0600

    footerTemplate dashboard

--- views/dashboard/index.php
+++ views/dashboard/index.php
@@ -63,5 +63,10 @@
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
 
-  </body>
-</html>
+<?php
+//Se incluye la clase con las plantillas del documento
+include('../../app/helpers/dashboard_page.php');
+//Se imprime el script para el movimiento del sidebar
+dashboard_Page::footerTemplate('index.js');
+dashboard_Page::sidebarTemplateMovement();
+?>
